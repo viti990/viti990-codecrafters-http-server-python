0efb3e85996c7643523a9c8006b816b7e9741cd7
Victor Augusto Cavalli <victoracavalli@gmail.com>
2023-11-05 20:05:23 -0300

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!!")
     HEADERS_END = '/r/n'*2
-    HTTP_200 = "HTTP/1.1 200 huehuebrbr" + HEADERS_END
+    HTTP_200 = "HTTP/1.1 200 OK" + HEADERS_END
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
