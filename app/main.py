e4eaf5ce1eb671eac5976a7f90f60b612a2f1176
Victor Augusto Cavalli <victoracavalli@gmail.com>
2023-11-05 20:12:08 -0300

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 # Uncomment this to pass the first stage
 import socket
 
+BUFFER = 1024
+CRLF = "\r\n"
+HEADERS_END = CRLF + CRLF
+HTTP_200 = "HTTP/1.1 200 OK" + HEADERS_END
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!!")
-    HEADERS_END = '/r/n'*2
-    HTTP_200 = "HTTP/1.1 200 OK" + HEADERS_END
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, addr = server_socket.accept()
-    #print(f"Connected by {addr}")
-    conn.recv(1024)
-    conn.send(HTTP_200.encode())
-    #conn.send(data.encode())
+    
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+        while True:
+            conn, address = server_socket.accept()
+            with conn:
+                chunk = conn.recv(BUFFER)
+                conn.send(HTTP_200.encode())
 
 
 if __name__ == "__main__":
