6bcb5fb1f35f8269afac304cb7b9f4556c8567ea
Victor Augusto Cavalli <victoracavalli@gmail.com>
2023-11-05 22:09:28 -0300

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -5,17 +5,27 @@ BUFFER = 1024
 CRLF = "\r\n"
 HEADERS_END = CRLF + CRLF
 HTTP_200 = "HTTP/1.1 200 OK" + HEADERS_END
+HTTP_404 = "HTTP/1.1 404 NOT FOUND" + HEADERS_END
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!!")
     
-    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+    with socket.create_server(("localhost", 4221)) as server_socket:
         while True:
             conn, address = server_socket.accept()
+            
             with conn:
                 chunk = conn.recv(BUFFER)
-                conn.send(HTTP_200.encode())
+                string_chunk = chunk.decode("utf-8")
+                start_line = string_chunk.split(CRLF)[0]
+                path = start_line.split(" ")[1]
+                if path == '/':
+                    print('200')
+                    conn.send(HTTP_200.encode())
+                else:
+                    print('404')
+                    conn.send(HTTP_404.encode())
 
 
 if __name__ == "__main__":
