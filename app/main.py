012e379ab8e5c1b3d11716b8c0ca4e152da4e50d
Victor Augusto Cavalli <victoracavalli@gmail.com>
2024-02-14 18:48:19 -0300

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -19,18 +19,16 @@ def main():
                 string_chunk = chunk.decode("utf-8")
                 start_line = string_chunk.split(CRLF)[0]
                 path = start_line.split(" ")[1]
-                if path[0] == '/':
+                if path == '/':
                     print('200')
-                    if len(path)-1 == 0:
-                        response = HTTP_200
-                    else:
+                    response = HTTP_200
+                elif path.startswith('/echo'):
                         response = HTTP_200[:-4] + "{0}Content-Type: text/plain{0}Content-Length: {1}{0}".format(CRLF, len(path[1:].split('/')[1])) + \
                         path[1:].split("/")[1]
-                    print(response.encode())
-                    conn.send(response.encode())
                 else:
                     print('404')
-                    conn.send(HTTP_404.encode())
+                    response = HTTP_404
+                conn.send(response.encode())
 
 
 if __name__ == "__main__":
